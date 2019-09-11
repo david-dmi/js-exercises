e6c0f7a4a2544f0856b1cfdccec1700535600a86
David Portilla <david_portilla@hotmail.com>
2019-09-11 09:43:14 -0500

    react course

---
+++ EDteam/React desde Cero/curso-react/clase2/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso'
+
+const App = () => (
+  <>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" alt="img-alt" src="http://images.unsplash.com/photo-1541702467897-41915a07d3a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Curso React</p>
+          <p>Subtitutlo JSX</p>
+          <a href="/" className="button third-color">Hola mundo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="ed-grid m-grid-3">
+    <Curso />
+    <Curso />
+    <Curso />
+  </div>
+  </>
+)
+
+export default App;
---
+++ EDteam/React desde Cero/curso-react/clase2/src/Curso.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Curso = () => (
+  <>
+  <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+      <img src="http://images.unsplash.com/photo-1541702467897-41915a07d3a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="img-tag"/>
+    </div>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+      <h3 className="t5 s-mb-2 s-center">
+        Programación orientada a objetos con Go
+      </h3>
+      <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+          <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
+              <img src="http://images.unsplash.com/photo-1541702467897-41915a07d3a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="img-tag"/>
+            </div>
+          </div>
+          <span className="small">Alexys Lozada</span>
+        </div>
+      </div>
+      <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="/">$ 20USD</a>
+      </div>
+    </div>
+  </article>
+  </>
+)
+
+export default Curso;
