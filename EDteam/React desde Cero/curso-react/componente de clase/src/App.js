f800f9eab8a4c6c9f7c4f5ecbf2336ec462ab69e
David Portilla <david_portilla@hotmail.com>
2019-09-20 09:34:17 -0500

    setState

--- EDteam/React desde Cero/curso-react/componente de clase/src/App.js
+++ EDteam/React desde Cero/curso-react/componente de clase/src/App.js
@@ -2,12 +2,11 @@ import React from 'react'
 import './styles/styles.scss'
 import Banner from './Banner'
 import Formulario from './Formulario'
-import Curso from './Curso'
+// import Curso from './Curso'
 
 const App = () => (
   <>
     <Banner />
-    <Curso />
     <Formulario />
   </>
 )
--- EDteam/React desde Cero/curso-react/componente de clase/src/Formulario.jsx
+++ EDteam/React desde Cero/curso-react/componente de clase/src/Formulario.jsx
@@ -1,25 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Formulario extends Component {
   constructor(props) {
     super(props)
+
+    this.state={
+      nombre: 'aaa',
+      correo: ''
+    }
+
+    this.cambiarNombre=this.cambiarNombre.bind(this)
+    this.cambiarCorreo=this.cambiarCorreo.bind(this)
+
   }
+
+  cambiarNombre (e) {
+    this.setState({
+      nombre: e.target.value,
+    })
+  }
+
+  cambiarCorreo (e) {
+    this.setState({
+      correo: e.target.value,
+    })
+  }
+
 
   render () {
     return (
       <>
-        <div> Hola Clase </div>
         <div className="ed-grid">
-          <form action="">
-            <div className="form-item">
-              <input type="text" name="" id="" />
+          <h1>Formulario</h1>
+          <form action="/">
+            <div className="ed-grid m-grid-2">
+              <div className="form__item">
+                <label htmlFor="nombre">Correo electronico</label>
+                <input type="text"
+                  onChange={ this.cambiarNombre }
+                />
+              </div>
+              <div className="form__item">
+                <label htmlFor="email">Correo electronico</label>
+                <input type="email" name="email" id=""
+                  onChange={ this.cambiarCorreo } />
+              </div>
+              <div className="form__item">
+                <input className="button full" type="submit" value="Enviar" />
+              </div>
             </div>
           </form>
+          <div>
+
+            <h3>{ `Hola ${ this.state.nombre }` }</h3>
+            <span>{ `Tu correo es ${ this.state.correo }` }</span>
+          </div>
+
+
         </div>
       </>
     )
   }
-
 }
 
 export default Formulario;
