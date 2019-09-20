786f3e30800d416d15125988a3dd58065749a8b7
David Portilla <david_portilla@hotmail.com>
2019-09-20 11:48:58 -0500

    update state components

--- EDteam/React desde Cero/curso-react/componente de clase/src/Formulario.jsx
+++ EDteam/React desde Cero/curso-react/componente de clase/src/Formulario.jsx
@@ -5,12 +5,14 @@ class Formulario extends Component {
     super(props)
 
     this.state = {
-      nombre: 'sin nombre',
-      correo: 'sin correo'
+      nombre: '',
+      correo: '',
+      fecha: new Date()
     }
 
     this.cambiarNombre = this.cambiarNombre.bind(this)
     this.cambiarCorreo = this.cambiarCorreo.bind(this)
+    this.cambiarFecha = this.cambiarFecha.bind(this)
 
   }
 
@@ -26,41 +28,63 @@ class Formulario extends Component {
     })
   }
 
+  cambiarFecha () {
+    this.setState({
+      fecha: new Date()
+    })
+
+  }
 
   render () {
     return (
-      <>
-        <div className="ed-grid">
-          <h1>Formulario</h1>
-          <form action="/">
-            <div className="ed-grid m-grid-2">
-              <div className="form__item">
-                <label htmlFor="nombre">Correo electronico</label>
-                <input type="text"
-                  onChange={ this.cambiarNombre }
-                />
-              </div>
-              <div className="form__item">
-                <label htmlFor="email">Correo electronico</label>
-                <input type="email" name="email" id=""
-                  onChange={ this.cambiarCorreo } />
-              </div>
-              <div className="form__item">
-                <input className="button full" type="submit" value="Enviar" />
-              </div>
+      <div className="ed-grid">
+        <h1>Formulario</h1>
+        <h4>Fecha actual: { Math.ceil(this.state.fecha / 1000) }</h4>
+        <form action="/">
+          <div className="ed-grid m-grid-2">
+            <div className="form__item">
+              <label htmlFor="nombre">Correo electronico</label>
+              <input type="text"
+                onChange={ this.cambiarNombre }
+              />
             </div>
-          </form>
-          <div>
-
-            <h3>{  `Hola ${ this.state.nombre }` }</h3>
-            <span>{ `Tu correo es ${ this.state.correo }` }</span>
-
+            <div className="form__item">
+              <label htmlFor="email">Correo electronico</label>
+              <input type="email" name="email" id=""
+                onChange={ this.cambiarCorreo } />
+            </div>
+            <div className="form__item">
+              <input className="button full" type="submit" value="Enviar" />
+            </div>
           </div>
-
+        </form>
+        <div>
+          <h3>{ `Hola ${ this.state.nombre }` }</h3>
+          <span>{ `Tu correo es ${ this.state.correo }` }</span>
         </div>
-      </>
+      </div>
     )
   }
+
+  componentDidMount () {
+    // let elemento = document.getElementById("elemento")
+    // console.log(elemento)
+    this.intervaloFecha = setInterval(() => {
+      this.cambiarFecha()
+      console.log(new Date())
+    }, 1000);
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    console.log(prevProps)
+    console.log(prevState)
+    console.log('-----')
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.intervaloFecha)
+  }
+
 }
 
 export default Formulario;
