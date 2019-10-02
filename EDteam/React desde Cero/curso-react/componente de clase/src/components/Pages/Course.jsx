591f93c4d1998b92d41778af348fa5a045063cdf
David Portilla <david_portilla@hotmail.com>
2019-10-02 09:26:54 -0500

    add Hook to update state

--- EDteam/React desde Cero/curso-react/componente de clase/src/components/Pages/Course.jsx
+++ EDteam/React desde Cero/curso-react/componente de clase/src/components/Pages/Course.jsx
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { useState } from 'react';
+// import axios from 'axios'
 
-class Course extends Component {
-  constructor(props) {
-    super(props)
+const Course = ({ match }) => {
+  const [state, setState] = useState({
+    id: 1,
+    title: "React desde cero",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    price: "38 USD",
+    teacher: "dportilla"
+  })
+  console.log(state)
 
-    this.state = {
-      cursoActual: {}
-    }
+  const changeTitle = (text) => {
+    setState({
+      ...state,
+      title: 'aaaaa'
+    })
   }
 
-  componentDidMount() {
-    axios.get(`http://my-json-server.typicode.com/david-portilla/json-db/cursos/${ this.props.match.params.id }`)
-      .then(res =>
-        this.setState({
-          cursoActual: res.data
-        })
-      )
-  }
-
-  render() {
-    return (
-      <div className="ed-grid m-grid-3">
-        {this.state.cursoActual ? (
-            <>
-              <h1 className="m-cols-3">Curso: {this.state.cursoActual.title} </h1>
-              <img className="m-cols-1" src={this.state.cursoActual.image} alt={this.state.cursoActual.title} />
-              <p className="m-cols-2">Profesor: {this.state.cursoActual.teacher} </p>
-            </>
-          ) :
-            <h1 className="m-cols-3">Curso no encontrado </h1>
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="ed-grid m-grid-3">
+      {
+        state ? (
+          <>
+            <h1 className="m-cols-3">Curso: {state.title} </h1>
+            <img className="m-cols-1" src={state.image} alt={state.title} />
+            <p className="m-cols-2">Profesor: {state.teacher} </p>
+            <button onClick={changeTitle.bind(this, "Go desde cero")}> Cambiar titulo </button>
+          </>
+        ) :
+          <h1 className="m-cols-3">Curso no encontrado </h1>
+      }
+    </div>
+  )
+  // }
 }
 
 export default Course
