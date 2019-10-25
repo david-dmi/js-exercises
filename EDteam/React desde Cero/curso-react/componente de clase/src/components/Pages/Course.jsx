7c406692df9619231d20271acbbc53ac9b092f5e
David Portilla <david_portilla@hotmail.com>
2019-10-25 14:25:17 -0500

    add Hook to Course.jsx

--- EDteam/React desde Cero/curso-react/componente de clase/src/components/Pages/Course.jsx
+++ EDteam/React desde Cero/curso-react/componente de clase/src/components/Pages/Course.jsx
@@ -2,23 +2,35 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const Course = ({ match }) => {
-  const [state, setState] = useState({})
+  const [course, setCourse] = useState({})
+  const [comment, setComment] = useState("Sin comentarios")
 
   useEffect(() => {
     console.log('aaa')
-    axios.get('http://my-json-server.typicode.com/david-portilla/json-db/cursos/2')
-      .then(res => setState(res.data))
+    axios.get(`http://my-json-server.typicode.com/david-portilla/json-db/cursos/${ match.params.id }`)
+      .then(res => setCourse(res.data))
   }, [])
+
+  const myComment = e => {
+    setComment(e.target.value)
+  }
 
   return (
     <div className="ed-grid m-grid-3">
       {
-        state ? (
-          <>
-            <h1 className="m-cols-3">Curso: {state.title} </h1>
-            <img className="m-cols-1" src={state.image} alt={state.title} />
-            <p className="m-cols-2">Profesor: {state.teacher} </p>
-          </>
+        course ? (
+          <div className="ed-grid">
+            <div>
+              <h1 className="m-cols-3">Curso: {course.title} </h1>
+              <img className="m-cols-1" src={course.image} alt={course.title} />
+              <p className="m-cols-2">Profesor: {course.teacher} </p>
+            </div>
+            <div>
+              <h2>Escribe tu comentario</h2>
+              <input type="text" placeholder="Escribe tu texto..." onChange={myComment.bind(this)} />
+              <p>{comment}</p>
+            </div>
+          </div>
         ) :
           <h1 className="m-cols-3">Curso no encontrado </h1>
       }
