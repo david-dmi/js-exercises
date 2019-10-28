91886e2f84ac750b514224669fc8ffd7b4eb2737
David Portilla <david_portilla@hotmail.com>
2019-10-28 12:21:16 -0500

    create Badge component

---
+++ Platzi/React JS/platzi-badges/src/components/Badge.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import confLogo from '../images/badge-header.svg'
+import './styles/Badge.css'
+
+export default class Badge extends Component {
+  render () {
+    return (
+      <div className="Badge">
+        {/* <h1>Badge Component</h1> */ }
+        <div className="Badge__header">
+          <img src={ confLogo } alt="Banner" />
+        </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar"
+            src="https://avatars3.githubusercontent.com/u/5439187?s=400&v=4" alt="Avatar" />
+          <h1>David <br />Portilla </h1>
+        </div>
+        <div className="Badge__section-info">
+          <h3> FE dev</h3>
+          <div> @david_portilla</div>
+        </div>
+        <div className="Badge__footer">
+          <p>#medellin #co</p>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ Platzi/React JS/platzi-badges/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from './components/Badge'
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
+
+const container = document.getElementById('app');
+
+ReactDOM.render(<Badge />, container);
