7554deaa8d74f3b829370b4b928c782ae9ce4e90
David Portilla <david_portilla@hotmail.com>
2019-12-17 09:45:13 -0500

    reverse commit

--- Catch-of-the-day/src/index.js
+++ Catch-of-the-day/src/index.js
@@ -1,8 +1,6 @@
 // let's go!
 import React from 'react'
 import { render } from "react-dom";
-// import StorePicker from "./components/StorePicker";
-// import App from "./components/App";
 import './css/style.css'
 import Router from "./components/Router";
 
